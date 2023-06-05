b1b0308a073d4709511d6c8f190105c555e9cd8c
fatihyenice <fatih.ydev@gmail.com>
2023-06-05 15:05:33 +0200

    Premier commit
    Création du repo

---
+++ variables.py
@@ -0,0 +1,44 @@
+# NOMBRE
+number1 = 123
+number2 = 5
+numberTotal = number1 / number2
+print(numberTotal)
+
+# NOMBRE A VIRGULE FLOTTANTE
+number3 = 3.14
+print(number3)
+
+# Chaine de caractèreœ
+text2 = "foo baz baz"
+print(text2)
+
+# boolean
+python_is_boring = True
+print(python_is_boring)
+
+python_is_cool = False
+print(python_is_cool)
+ 
+# VÉRIFICATION DU TYPE DE DONNÉES
+# MOTS CLÉS = TYPE()
+a = "Hello"
+print(type(a))
+
+# VÉRIFICATION DU TYPE DE DONNÉES
+b =  15
+print(type(b) is str)
+print(type(b) is int)
+
+# TRANSTYPAGE
+number_transtyp = 123
+print(type(str(number_transtyp)))
+print(str(number_transtyp))
+
+# TRANSTYPAGE
+Texte = "123456789"
+
+# LES FONCTIONS DE TRANSTYPAGE
+# str() convertit vers string (chaine de caractère)
+# int() convertir vers integer (nombre)
+# float() convertit vers float (nombre à virgule)
+# bool() convertit vers boolean (0 ou 1 | false or true)
