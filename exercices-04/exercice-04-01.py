487c93a971a5d356bd8f3fbdc22fd8385f9b0ed8
fatihyenice <fatih.ydev@gmail.com>
2023-06-07 09:34:29 +0200

    AJOUT DE LEXO 4

---
+++ exercices-04/exercice-04-01.py
@@ -0,0 +1,16 @@
+# exo 4.1
+# écrivez un bloc if qui affiche
+# - "le nombre est égale à 1" si la variable number contient un 1
+# - "le nombre est différent de 1" si la variable number ne contient pas de 1
+
+import random
+
+# affectaction d'un nombre aléatoire entre 0 et 5 inclus à la variable number
+number = random.randint(0, 5)
+print(number)
+if(number == 1):
+    print('le nombre est égale à 1')
+else:
+    print('le nombre est différent de 1')
+# réponse 4.1
+
---
+++ exercices-04/exercice-04-05.py
@@ -0,0 +1,16 @@
+# exo 4.5
+# écrivez un bloc if qui affiche
+# - "le nombre est compris entre 0 et 49 inclus" si la variable number contient une valeur comprise entre 0 et 49
+# - "le nombre n'est pas compris entre 0 et 49 inclus" si la variable number ne contient pas de valeur comprise entre 0 et 49
+
+import random
+
+# affectaction d'un nombre entier entre 0 et 99 à la variable number
+number = random.randint(0, 99)
+print(number)
+if(number > 0 and number < 49):
+    print('le nombre est compris entre 0 et 49 inclus')
+else:
+    print("le nombre n'est pas compris entre 0 et 49 inclus")
+# réponse 4.5
+
