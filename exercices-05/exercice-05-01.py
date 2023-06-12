e67ecb7d7116749e794ab6916a8fefaac9a3e8c8
fatihyenice <fatih.ydev@gmail.com>
2023-06-12 16:25:21 +0200

    EXERCICES 05 COMPLET

---
+++ exercices-05/exercice-05-01.py
@@ -0,0 +1,14 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+
+def multiplication(a: float, b: float) -> float:
+    """
+    CECI est un TESTE
+
+    """
+    return a * b
+help(multiplication)
+
