186d96c65bf8c1b46a9940f78ee8190c2d39289e
fatihyenice <fatih.ydev@gmail.com>
2023-06-14 14:19:36 +0200

    FIN DU DOSSIER 06

---
+++ exercices-06/exercice-06-18.py
@@ -0,0 +1,21 @@
+# exo 6.18
+# Avec le même tableau en 2 dimensions, affichez toutes les valeurs plus petites ou égales à 50 ainsi que leur cordoonnées (ligne et colonne)
+# Vous pouvez réutiliser la variable `size` qui a permis de construire le tableau en 2 dimensions
+#
+# ATTENTION : cet exercice nécessite l'utilisation d'une boucle `for`
+
+import random
+
+size = 5
+matrix = []
+
+for row in range(0, size):
+    row = [random.randint(40, 100) for row in range(0, size)]
+    matrix.append(row)
+
+print(matrix)
+
+# réponse 6.18
+for i in range(0, 51):
+    for j in range(0, 51):
+        print(i, j)
